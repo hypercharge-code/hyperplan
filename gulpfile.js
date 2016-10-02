be21fff1b0be7d0119d046a656801d654ee27744
Citizen <git@10citylabs.xyz>
2016-10-02 15:39:42 +0100

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var rename = require('gulp-rename');
+var del = require('del');
+
+var sassOptions = {
+  errLogToConsole: true,
+  outputStyle: 'expanded'
+};
+
+gulp.task('clean', function(done) {
+    del(['dist/'], done);
+});
+
+gulp.task('build', function () {
+  return gulp
+    .src('src/**/*.scss')
+    .pipe(sass(sassOptions).on('error', sass.logError))
+    .pipe(postcss([require('autoprefixer')({ browsers: [
+      '>1%',
+      'last 4 versions',
+      'Firefox ESR',
+      'not ie < 9']
+    })]))
+    .pipe(gulp.dest('dist/'))
+    .pipe(postcss([ require('cssnano')() ]))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('dist/'));
+});
